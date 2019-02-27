ef6253584bf53f6438af1080cf2cc0fbb485f3d0
Gabriele Nosso <gabrielenosso@xrite.com>
2019-02-27 18:49:39 +0100

    Updated Pantone API info with PAntone API beta

--- javascript/config.js
+++ javascript/config.js
@@ -1,13 +1,15 @@
 export default {
-  // Values to update with Pantone API values
-  REGION: 'eu-central-1',
-  API_ENDPOINT: 'https://2kotxbvfc5dhfmdsywvntcjqnu.appsync-api.eu-central-1.amazonaws.com/graphql',
-  IDENTITY_POOL_ID: 'eu-central-1:ed6e3ac3-eafd-44f9-97a0-0dd3ada6d3ae',
-  USER_POOL_ID: 'eu-central-1_QyJ2z85xR',
-  CLIENT_ID: '5rplgkpl99uor6hdsc6aidjlmq',
+  // Pantone Authentication data
+  IDENTITY_POOL_ID: 'eu-central-1:dc85d588-71b3-46ee-bbe6-af24a8957add',
+  USER_POOL_ID: 'eu-central-1_furWNPtEv',
+  CLIENT_ID: '77g6o30bt4e3io00qpnv09nf0',
   AUTH_TYPE: 'AWS_IAM',
+  REGION: 'eu-central-1',
+
+  // Pantone API data
+  API_ENDPOINT: 'https://ajwuyywqbzdpvnymnmwg7o3xq4.appsync-api.eu-central-1.amazonaws.com/graphql',
 
   // User credentials
-  USERNAME: 'gabrielenosso@gmail.com',
-  PASSWORD: 'Password0',
+  USERNAME: 'pantone.api.beta@gmail.com',
+  PASSWORD: 'tryAPIbeta1',
 }
